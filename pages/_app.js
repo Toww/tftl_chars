ef0449f36f030c172e9ef2e3c9374cb4bd340900
T <25932452+Toww@users.noreply.github.com>
2020-11-29 11:36:08 +0100

    Tailwind setup OK

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "styles/main.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto my-4">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
